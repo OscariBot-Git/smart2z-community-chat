7b01bb7cfc0fcbd085aba9ee5f5045cc78cd7f34
Oscar Sunday <smart2zmail@gmail.com>
2026-04-24 16:40:08 +0100

    update

--- server.js
+++ server.js
@@ -46,12 +46,22 @@ const messageSchema = new mongoose.Schema({
   edited: Boolean,
   deleted: Boolean,
   reactions: Object,
-  avatar: String,
   online: Number
 });
 
 messageSchema.index({ type: 1, timestamp: -1 });
 const Message = mongoose.model('Message', messageSchema);
+
+
+
+// =====================
+// AVATAR SCHEMA
+// =====================
+const userSchema = new mongoose.Schema({
+  username: { type: String, unique: true, required: true },
+  avatar: {type: String, default: "" }
+});
+
 
 
 // =====================
@@ -438,6 +448,26 @@ io.on('connection', (socket) => {
     if (!socket.username) return;
     socket.broadcast.emit("stop typing", { username: socket.username });
   });
+
+
+  // =====================
+  // 🚪 UPDATE AVATAR
+  // =====================
+	socket.on("save avatar", async ({ username, avatar }) => {
+	  await User.updateOne({ username }, { $set: { avatar } });
+	  io.emit("avatar updated", { username, avatar });
+	});
+
+
+  // =====================
+  // 🚪 GET AVATAR
+  // =====================
+	socket.on("get users", async () => {
+	  const users = await User.find({}, "username avatar");
+	  socket.emit("users list", users);
+	});
+
+
 
   // =====================
   // 🚪 DISCONNECT
