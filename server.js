5969fff82b9e8a9a2dbc7b8369aad2fc525ff3ab
Oscar Sunday <smart2zmail@gmail.com>
2026-05-06 23:22:46 +0100

    update

--- server.js
+++ server.js
@@ -27,12 +27,18 @@ mongoose.connect(MONGO_URI)
     for (const type of TYPES) {
       await trimByType(type, getLimitByType(type));
     }
-   /* await Message.syncIndexes(); // ✅ TURN ON ONCE WHEN SCHEMA CHANGE 
+   await Message.syncIndexes(); // ✅ TURN ON ONCE WHEN SCHEMA CHANGE 
     await Meta.updateOne(
-	  { key: "users_version" },
+	  { key: "news_version" },
 	  { $setOnInsert: { value: 1 } },
 	  { upsert: true }
-	); */
+	);
+	
+	await Meta.updateOne(
+	  { key: "announcement_version" },
+	  { $setOnInsert: { value: 1 } },
+	  { upsert: true }
+	);
    
   })
   .catch(err => console.error("❌ MongoDB error:", err));
