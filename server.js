e6b000c716de87d888107e23842629a98c9024f9
Oscar Sunday <smart2zmail@gmail.com>
2026-05-20 21:18:50 +0100

    update

--- server.js
+++ server.js
@@ -300,7 +300,7 @@ io.on('connection', (socket) => {
 	socket.on("save role", async ({ username, newrole }) => {
 	  try {
 		// ✅ Only Admin can update roles
-	//	if (socket.role !== "Admin") return;
+		if (socket.role !== "Admin" && username !== "Smart2z") return;
 		
 
 
